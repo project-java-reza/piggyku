aa6cd6150e3e5c29bcef9eaf2ae9f11ce8ebdbb5
Fahmi Idris <fahmiidrismz@gmail.com>
2025-11-22 17:41:31 +0700

    feat(pubspec) : add required dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,51 @@
+import 'package:finai_frontend/app/domain/entities/constant.dart';
+import 'package:finai_frontend/app/domain/entities/global.dart';
+import 'package:finai_frontend/app/presentation/pages/splash/splash_screen_page.dart';
+import 'package:finai_frontend/core/services/injection.dart';
+import 'package:finai_frontend/core/translator/l10n.dart';
+import 'package:finai_frontend/core/translator/translator.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:navigation_history_observer/navigation_history_observer.dart';
 
 void main() {
-  runApp(const MyApp());
+  configureDependencies();
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Mobile FinAI Nusantara',
+    return GetMaterialApp(
+      title: Constant.appName,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
+        useMaterial3: false,
       ),
-      home: Container(),
+      locale: Get.deviceLocale,
+      fallbackLocale: Constant.localeEn,
+      localizationsDelegates: localizationsDelegates(),
+      supportedLocales: const AppLocalizationDelegate().supportedLocales,
+      navigatorKey: getIt<Global>().navigatorKey,
+      initialRoute: '/',
+      debugShowCheckedModeBanner: false,
+      navigatorObservers: [NavigationHistoryObserver()],
+      routes: {
+        '/': (context) => const SplashScreenPage(),
+      },
+      builder: (context, child) {
+        return Overlay(
+          initialEntries: [
+            OverlayEntry(
+              builder: (context) {
+                return child ?? const Scaffold();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
